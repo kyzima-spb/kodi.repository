32bbbe32e900381a77f32f555896248e3246d992
Kirill Vercetti <office@kyzima-spb.com>
2025-01-12 18:08:21 +0300

    Добавлен диалог ввода строки и алиас для ОК диалога

---
+++ addons/script.module.kodi_useful/lib/kodi_useful/gui.py
@@ -0,0 +1,64 @@
+import typing as t
+
+import xbmc
+import xbmcgui
+
+
+__all__ = ('alert', 'prompt')
+
+
+class PromptResult(t.NamedTuple):
+    value: t.Any
+    canceled: bool = False
+
+    def __bool__(self) -> bool:
+        return not self.canceled
+
+
+def alert(title: str, message: str) -> bool:
+    return xbmcgui.Dialog().ok(title, message)
+
+
+def prompt(
+    msg: str,
+    required: bool = False,
+    default: t.Optional[t.Any] = None,
+    type_cast: t.Callable[[str], t.Any] = None,
+    hidden: bool = False,
+) -> PromptResult:
+    """
+    Запрашивает данные от пользователя и возвращает ввод.
+
+    Arguments:
+        msg (str): строка приглашения.
+        required (bool): требуется обязательный ввод, если не указано значение по умолчанию.
+        default (mixed): значение, которое будет использовано в случае пустого ввода.
+        type_cast (callable): функция обратного вызова для обработки введенного значения.
+        input_type (int): внешний вид поля ввода в Kodi.
+    """
+    if default is not None:
+        required = False
+
+    keyboard = xbmc.Keyboard('' if default is None else str(default), msg, hidden)
+
+    while 1:
+        keyboard.doModal()
+
+        if not keyboard.isConfirmed():
+            return PromptResult(default, True)
+
+        value = keyboard.getText().strip()
+
+        if not value:
+            if not required:
+                return PromptResult(default)
+
+            alert('Error', 'Input required')
+        else:
+            if type_cast is None:
+                return PromptResult(value)
+
+            try:
+                return PromptResult(type_cast(value))
+            except ValueError as e:
+                alert('Error', str(e))
