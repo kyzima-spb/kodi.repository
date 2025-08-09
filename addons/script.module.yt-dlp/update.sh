4f350f688db60888c23f76de0040ddbb23633cb0
Kirill Vercetti <office@kyzima-spb.com>
2025-08-10 01:03:12 +0300

    Update script for bump yt-dlp version

--- addons/script.module.yt-dlp/update.sh
+++ addons/script.module.yt-dlp/update.sh
@@ -16,6 +16,8 @@ fi
 
 python3 -m pip install -t "${SCRIPT_DIR}/lib" --upgrade "$package" && \
   version="$(PYTHONPATH="${SCRIPT_DIR}/lib" python3 -m pip show "$package" | awk '/^Version:/{print $2}')" && \
+  suffix="$(xmlstarlet sel -t -v "/addon/@version" "${SCRIPT_DIR}/addon.xml" | awk -F- '{print $2}')" && \
+  [ -n "$suffix" ] && version="${version}-$suffix" || true && \
   xmlstarlet ed -L -u "/addon/@version" -v "$version" "${SCRIPT_DIR}/addon.xml" && \
   cd "${SCRIPT_DIR}/lib" && \
   find . -mindepth 1 -maxdepth 1 -not -path "./yt_dlp" -not -path "./yt_dlp_utils" -exec rm -rf {} +
